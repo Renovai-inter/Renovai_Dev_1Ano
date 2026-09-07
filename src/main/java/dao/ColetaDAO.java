2fc0a10058a47da010f10e162a3905383ebf8eab
Luiza Caravaggio <luiza.20260177@institutojef.ogr.br>
2026-09-07 19:03:01 -0300

    feat: iniciando o create do cooperado e da coleta

--- src/main/java/dao/ColetaDAO.java
+++ src/main/java/dao/ColetaDAO.java
@@ -2,6 +2,10 @@ package dao;
 
 import util.Conexao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ColetaDAO {
 
     // ATRIBUTOS
@@ -15,6 +19,32 @@ public class ColetaDAO {
 
     // === METODOS CREATE ==============================================================================================
 
+    public int registrarColeta(String tipo, String status, String origem_entrega, String nome_local_origem, String observacoes){
+
+        String sql = "INSERT INTO coleta(id_coleta, id_cooperativa, tipo, status, id_rota, id_endereco_rota, id_cooperado_responsavel," +
+                "origem_entrega, nome_local_origem, data_agendada, data_inicio, data_fim, peso_total_kg, observacoes) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+        Connection conexao = conn.conectar();
+
+        try{
+            PreparedStatement pstm = conexao.prepareStatement(sql);
+            //valores esperados pela query
+            pstm.setString(1, tipo);
+            pstm.setString(2, status);
+            pstm.setString(3, origem_entrega);
+            pstm.setString(4, nome_local_origem);
+            pstm.setString(5, observacoes);
+            //executando a query
+            pstm.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            conn.desconectar(conexao);
+        }
+        return 0;
+    }
+
+    }
 
 
     // === METODOS READ ================================================================================================
@@ -29,4 +59,4 @@ public class ColetaDAO {
 
 
 
-}
+
